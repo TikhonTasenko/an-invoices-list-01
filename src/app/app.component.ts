59ae5615118a139c509e71c64e76eaf305ffd0eb
Tikhon Tasenko <t.tasenko@gmail.com>
2022-06-13 16:51:03 +1200

    Implemented AG Grid in standard Angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { ColDef } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'an-invoices-list-01';
+  rowData$! : Observable<any[]>;
+
+  colDefs: ColDef[] = [
+    {field:'id'},
+    {field:'status'},
+    {field:'date/time'},
+    {field:'client'}
+  ];
+
+  defaultColDef: ColDef = {
+    sortable: true, filter: true
+  }
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.rowData$ = this.http.get<any[]>('https://angular-test-02.s3.ap-southeast-2.amazonaws.com/data.json');
+    //this.rowData$ = this.http.get<any[]>('assets/data.json');
+  }
 }
